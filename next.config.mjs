09fd3f5afb987b6868979d54666a94fb1efe6b2d
Dann1y <leedanny0102@gmail.com>
2023-11-22 23:23:39 +0900

    fix: add dependencies & setting

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+import rehypePrism from "@mapbox/rehype-prism";
+import nextMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  experimental: {
+    esmExternals: "loose",
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
+};
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+});
+
+export default withMDX(nextConfig);
